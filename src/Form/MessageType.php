c343f4a5bd09c42c7eaf0ff57825060d705a98fa
maitrecanard <mathieusiaudeau@gmail.com>
2022-12-21 17:14:46 +0100

    create MainController & create form message

---
+++ src/Form/MessageType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Messages;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MessageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('content')
+            ->add('status')
+            ->add('created_at')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Messages::class,
+        ]);
+    }
+}
