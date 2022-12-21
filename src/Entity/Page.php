7365152076ca9c5fcf0960863e8ae6f73af73b5e
maitrecanard <mathieusiaudeau@gmail.com>
2022-12-21 16:29:59 +0100

    maj-page_create_message

--- src/Entity/Page.php
+++ src/Entity/Page.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\PagesRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PageRepository::class)]
@@ -13,8 +14,38 @@ class Page
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
     }
 }
