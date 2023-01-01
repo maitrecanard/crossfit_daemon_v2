8d7cdb11ed3a6c804d631d53792cd596a7881a77
maitrecanard <mathieusiaudeau@gmail.com>
2023-01-01 11:42:22 +0100

    final_auth

--- src/Controller/Back/DashBoardController.php
+++ src/Controller/Back/DashBoardController.php
@@ -15,20 +15,14 @@ class DashBoardController extends AbstractController
     /**
      * @Route("/admin", name="dashboard")
      */
-    public function index(VisitorCounter $visitor, ProductsCounter $products, ProductsRepository $productsRepository): Response
+    public function index(VisitorCounter $visitor,): Response
     {
         
         $countVisitor = $visitor->getCountVisitor();
-        $bestProduct = $products->getBestproduct();
-        $activProducts = $products->getCountProductsStatus(1);
-        $inactivProducts = $products->getCountProductsStatus(0);
-        $products =  $productsRepository->findAll();
+        
         return $this->render('back/main/index.html.twig', [
             'countVisitor' => $countVisitor,
-            'bestProduct' => $bestProduct,
-            'activProducts' => $activProducts,
-            'inactivProducts' => $inactivProducts,
-            'products' => $products
+            
         ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -34,6 +34,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'boolean')]
     private $isVerified = false;
 
+    #[ORM\Column(length: 50)]
+    private ?string $lastname = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $firstname = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -131,6 +137,30 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setIsVerified(bool $isVerified): self
     {
         $this->isVerified = $isVerified;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
 
         return $this;
     }
